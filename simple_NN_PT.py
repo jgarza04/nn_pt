bcc28d48efda6379d174636adb3955e828a4453e
Juan Pablo Garza <juanpablogarza@server.ca>
2025-11-03 13:14:14 -0800

    changes:

--- simple_NN_PT.py
+++ simple_NN_PT.py
@@ -20,11 +20,82 @@ class NN(nn.Module):
         x = self.fc2(x)
         return x
 
+"""
 model = NN(784, 10)
 x = torch.randn(64, 784)
 print(model(x).shape) # [64, 10]
+"""
 
 # Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Hyperparameters
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 10
 
 # Load Data
+train_dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = datasets.MNIST(root="dataset/", train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+# Initialize Network
+model = NN(input_size= input_size, num_classes= num_classes).to(device)
+
+# Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train Network
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # If possible, get data to cuda
+        data = data.to(device)
+        targets = targets.to(device=device)
+        
+        data = data.reshape(data.shape[0], -1)    
+
+        # Forward pass
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # Backward pass
+        optimizer.zero_grad() # Reset gradients to 0
+        loss.backward() # Calling backward propagation
+
+        # Gradient descent or Adam step
+        optimizer.step() # Update weights and biases depending on the optimizer
+
+# Check accuracy on training & test to see how good our model is
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print("Checking accuracy on training data")
+    else:
+        print("Checking accuracy on test data")
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad(): # Let pytorch know that we are not computing gradients in calculations
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}")
+    
+    model.train()
+    return num_correct / num_samples
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader, model)
